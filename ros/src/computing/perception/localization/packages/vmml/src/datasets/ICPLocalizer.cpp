e8b8c7a716c9eae228df7b9863579c4ee3bab59b
Adi Sujiwo <sujiwo@gmail.com>
2018-12-04 09:49:55 +0900

    Stub files for ICP

---
+++ ros/src/computing/perception/localization/packages/vmml/src/datasets/ICPLocalizer.cpp
@@ -0,0 +1,18 @@
+/*
+ * ICPLocalizer.cpp
+ *
+ *  Created on: Dec 4, 2018
+ *      Author: sujiwo
+ */
+
+#include <src/datasets/ICPLocalizer.h>
+
+ICPLocalizer::ICPLocalizer() {
+	// TODO Auto-generated constructor stub
+
+}
+
+ICPLocalizer::~ICPLocalizer() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ ros/src/computing/perception/localization/packages/vmml/src/datasets/ICPLocalizer.h
@@ -0,0 +1,39 @@
+/*
+ * ICPLocalizer.h
+ *
+ *  Created on: Dec 4, 2018
+ *      Author: sujiwo
+ */
+
+#ifndef _ICPLOCALIZER_H_
+#define _ICPLOCALIZER_H_
+
+
+#include <string>
+
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/registration/icp.h>
+
+#include "utilities.h"
+
+
+
+class ICPLocalizer
+{
+public:
+	ICPLocalizer();
+	virtual ~ICPLocalizer();
+
+	void loadMap (const std::string &filename);
+
+	void putEstimation (const Pose &pEst);
+
+	Pose localize (pcl::PointCloud<pcl::PointXYZ>::ConstPtr scan);
+
+protected:
+
+	pcl::IterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ> mIcp;
+};
+
+#endif /* _ICPLOCALIZER_H_ */
