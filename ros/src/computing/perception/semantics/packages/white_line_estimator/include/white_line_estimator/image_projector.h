315ca3dbdc42c8772427da6882628eb4bdcf85c6
MasayaKataoka <ms.kataoka@gmail.com>
2019-01-06 03:30:54 +0900

    add pcl to the depends

--- ros/src/computing/perception/semantics/packages/white_line_estimator/include/white_line_estimator/image_projector.h
+++ ros/src/computing/perception/semantics/packages/white_line_estimator/include/white_line_estimator/image_projector.h
@@ -8,8 +8,17 @@
 //headers in OpenCV
 #include <opencv2/imgproc/imgproc.hpp>
 
-//headers in STL
-#include <map>
+//headers in PCL
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/ModelCoefficients.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/filters/extract_indices.h>
+#include <pcl/filters/project_inliers.h>
 
 class ImageProjector
 {
