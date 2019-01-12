b8cdd4d447252be55056ee57d35ed49f63a0d5ea
Masaya Kataoka <ms.kataoka@gmail.com>
2019-01-12 16:25:31 +0900

    add project function

--- ros/src/computing/perception/semantics/packages/white_line_estimator/include/white_line_estimator/image_points_projector.h
+++ ros/src/computing/perception/semantics/packages/white_line_estimator/include/white_line_estimator/image_points_projector.h
@@ -1,19 +1,34 @@
 #ifndef IMAGE_POINTS_PROJECTOR_H_INCLUDED
 #define IMAGE_POINTS_PROJECTOR_H_INCLUDED
 
+//headers in opencv
+#include <opencv2/imgproc/imgproc.hpp>
+
 //headers in Autoware
 #include <white_line_estimator/cv_camera_info.h>
 
 //headers in boost
 #include <boost/optional.hpp>
 
+//headers in STL
+#include <mutex>
+
+//headers in ROS
+#include <ros/ros.h>
+#include <geometry_msgs/Point.h>
+
 class ImagePointsProjector
 {
 public:
     ImagePointsProjector();
     ~ImagePointsProjector();
+    boost::optional<std::vector<geometry_msgs::Point> > project(std::vector<cv::Point> image_points);
 private:
-    //boost::optional<>
+    void setCameraInfo(sensor_msgs::CameraInfo info);
+    void setProjectionMatrix(autoware_msgs::ProjectionMatrix proj_matrix);
+    boost::optional<CvCameraInfo> camera_info_;
+    boost::optional<CvProjMatrix> proj_matrix_;
+    std::mutex mtx_;
 };
 
 #endif  //IMAGE_PROJECTOR_H_INCLUDED
--- ros/src/computing/perception/semantics/packages/white_line_estimator/src/image_points_projector.cpp
+++ ros/src/computing/perception/semantics/packages/white_line_estimator/src/image_points_projector.cpp
@@ -9,3 +9,34 @@ ImagePointsProjector::~ImagePointsProjector()
 {
     
 }
+
+boost::optional<std::vector<geometry_msgs::Point> > ImagePointsProjector::project(std::vector<cv::Point> image_points)
+{
+    std::lock_guard<std::mutex> lock(mtx_);
+    if(!camera_info_)
+    {
+        ROS_ERROR_STREAM("cameara info does not recieved.");
+        return boost::none;
+    }
+    if(!proj_matrix_)
+    {
+        ROS_ERROR_STREAM("projection matrix does not recieved.");
+        return boost::none;
+    }
+    std::vector<geometry_msgs::Point> projected_points;
+    return projected_points;
+}
+
+void ImagePointsProjector::setCameraInfo(sensor_msgs::CameraInfo info)
+{
+    std::lock_guard<std::mutex> lock(mtx_);
+    camera_info_ = CvCameraInfo(info);
+    return;
+}
+
+void ImagePointsProjector::setProjectionMatrix(autoware_msgs::ProjectionMatrix proj_matrix)
+{
+    std::lock_guard<std::mutex> lock(mtx_);
+    proj_matrix_ = CvProjMatrix(proj_matrix);
+    return;
+}
