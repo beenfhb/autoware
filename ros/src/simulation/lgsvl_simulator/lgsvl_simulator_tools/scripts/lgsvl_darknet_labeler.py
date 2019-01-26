7c6ddcba3f8c8cf4f7752a2cf7859b9293aa8580
Masaya Kataoka <ms.kataoka@gmail.com>
2019-01-26 17:39:14 +0900

    enables make data dirs by python node

--- ros/src/simulation/lgsvl_simulator/lgsvl_simulator_tools/scripts/lgsvl_darknet_labeler.py
+++ ros/src/simulation/lgsvl_simulator/lgsvl_simulator_tools/scripts/lgsvl_darknet_labeler.py
@@ -4,6 +4,7 @@ import rospkg
 from lgsvl_msgs.msg import Detection2DArray
 import os
 import datetime
+import shutil
 
 class LgsvlDarknetLabaler:
     def __init__(self):
@@ -12,11 +13,18 @@ class LgsvlDarknetLabaler:
         time_str = datetime.datetime.now().strftime('%Y_%m_%d_%H_%M_%S')
         self.data_dir = self.package_path+"/"+time_str+"_data"
         os.mkdir(self.data_dir)
-        os.mkdir(self.data_dir+"/cfg")
+        os.mkdir(self.data_dir+"/data")
         self.datknet_path = rospack.get_path('vision_darknet_detect') + "/darknet"
         object_types = "car\npedestrian\nbicycle"
-        with open(self.data_dir+"/cfg/lgsvl.cfg", mode='w') as f:
+        os.mkdir(self.data_dir+"/data/1car")
+        os.mkdir(self.data_dir+"/data/2pedestrian")
+        os.mkdir(self.data_dir+"/data/3bicycle")
+        with open(self.package_path+"/template/class/class.txt", mode='w') as f:
             f.write(object_types)
+        with open(self.package_path+"/template/cfg/lgsvl.names", mode='w') as f:
+            f.write(object_types)
+        shutil.copytree(self.package_path+"/template/cfg", self.data_dir+"/cfg")
+        shutil.copyfile(self.package_path+"/template/class/class.txt", self.data_dir+"/data/class.txt")
         self.sub = rospy.Subscriber("/simulator/ground_truth/2d_detections", Detection2DArray, self.callback)
     def callback(self,data):
         rospy.loginfo(rospy.get_caller_id()+"I heard %s",data.data)
