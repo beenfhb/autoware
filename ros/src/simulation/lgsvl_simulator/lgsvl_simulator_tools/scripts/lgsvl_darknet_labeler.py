d72ef5cd254b0cd8f0132abd8ea772544a498424
Masaya Kataoka <ms.kataoka@gmail.com>
2019-01-26 17:15:17 +0900

    add message filters to the exec depend

--- ros/src/simulation/lgsvl_simulator/lgsvl_simulator_tools/scripts/lgsvl_darknet_labeler.py
+++ ros/src/simulation/lgsvl_simulator/lgsvl_simulator_tools/scripts/lgsvl_darknet_labeler.py
@@ -10,9 +10,13 @@ class LgsvlDarknetLabaler:
         rospack = rospkg.RosPack()
         self.package_path = rospack.get_path('lgsvl_simulator_tools')
         time_str = datetime.datetime.now().strftime('%Y_%m_%d_%H_%M_%S')
-        os.mkdir(self.package_path+"/"+time_str+"_data")
+        self.data_dir = self.package_path+"/"+time_str+"_data"
+        os.mkdir(self.data_dir)
+        os.mkdir(self.data_dir+"/cfg")
         self.datknet_path = rospack.get_path('vision_darknet_detect') + "/darknet"
-        object_types = "car\n"
+        object_types = "car\npedestrian\nbicycle"
+        with open(self.data_dir+"/cfg/lgsvl.cfg", mode='w') as f:
+            f.write(object_types)
         self.sub = rospy.Subscriber("/simulator/ground_truth/2d_detections", Detection2DArray, self.callback)
     def callback(self,data):
         rospy.loginfo(rospy.get_caller_id()+"I heard %s",data.data)
